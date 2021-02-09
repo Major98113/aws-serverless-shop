41ae85492fdf5afdc2a0586442f30bc6d898dba0
Maksim Mozhorov <Maksim_Mozhorov@epam.com>
2021-02-09 21:42:08 +0300

    Added import-service

--- product-service/lambdas/getAllProducts.ts
+++ product-service/lambdas/getAllProducts.ts
@@ -1,7 +1,7 @@
 import { diContainer } from "../../libs/di/inversify.config";
 import { DBInterface } from "../../libs/db/DB.interface";
 import { TYPES } from "../../libs/types";
-import { ProductsModel, ProductInterface } from "../models/products.model";
+import { ProductsModel, ProductInterface } from "../../libs/models/products.model";
 import { errorResponse, successResponse, responseInterface } from "../../libs/response-helpers";
 
 export const getAllProducts: ( event, _context ) => Promise<responseInterface> = async (event, _context) => {
