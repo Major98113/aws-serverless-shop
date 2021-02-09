83a8ab725bb124ce240430733f7aedf428f0ccec
Maksim Mozhorov <Maksim_Mozhorov@epam.com>
2021-02-09 21:26:19 +0300

    Added import service

---
+++ import-service/serverless.ts
@@ -0,0 +1,52 @@
+import type { Serverless } from 'serverless/aws';
+
+const serverlessConfiguration: Serverless = {
+  service: {
+    name: 'import-service',
+    // app and org for use with dashboard.serverless.com
+    // app: your-app-name,
+    // org: your-org-name,
+  },
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  // Add the serverless-webpack plugin
+  plugins: ['serverless-webpack', 'serverless-offline'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '10',
+      DB_HOST: 'DB_HOST',
+      DB_PORT: 'DB_PORT',
+      DB_NAME: 'DB_NAME',
+      DB_USERNAME: 'DB_USERNAME',
+      DB_PASSWORD: 'DB_PASSWORD'
+    },
+  },
+  package: {
+    include: [ '../libs/*' ]
+  },
+  functions: {
+    importProductsFile: {
+      handler: 'handler.importProductsFile',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'import',
+          }
+        }
+      ]
+    }
+  }
+}
+
+module.exports = serverlessConfiguration;
